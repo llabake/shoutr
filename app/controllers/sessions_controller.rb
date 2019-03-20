1d8e6818773bedf6d68a0564c1b838bac72dc10e
llabake <lemboyelabake@gmail.com>
2019-03-20 19:59:35 +0100

    redirect to dashboard when signed in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < Clearance::SessionsController
   private
+
   def authenticate(_)
     super(session_params)
   end
@@ -9,15 +10,17 @@ class SessionsController < Clearance::SessionsController
   end
 
   def session_params_with_email
-    params.require(:session).permit(:password).merge(email: user.email)
+    params.
+        require(:session)
+        .permit(:password)
+        .merge(email: user.email)
   end
 
   def user
-    User.where(email: email_or_username)
-        .or(User.where(username: email_or_username)).first || Guest.new
+    User.where(email: email_or_username).or(User.where(username: email_or_username)).first || Guest.new
   end
 
   def email_or_username
-
+    params[:session][:email_or_username]
   end
 end
