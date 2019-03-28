6dc0a935db5e7dcdec1663e896d2623ad9f234dd
llabake <lemboyelabake@gmail.com>
2019-03-28 18:25:44 +0100

    add ability to like and unlike shouts

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,12 @@ Rails.application.routes.draw do
     root to: 'dashboards#show'
   end
   root to: 'home#show'
-  resources :shouts, only: %i[create show]
+  resources :shouts, only: %i[create show] do
+    member do
+      post 'like' => 'likes#create'
+      delete 'unlike' => 'likes#destroy'
+    end
+  end
   resources :passwords, controller: 'clearance/passwords', only: %i[create new]
   resource :session, only: [:create]
 
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,47 +10,60 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_190_327_193_501) do
+ActiveRecord::Schema.define(version: 2019_03_28_170900) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'photo_shouts', force: :cascade do |t|
-    t.string 'image_file_name', null: false
-    t.string 'image_content_type', null: false
-    t.bigint 'image_file_size', null: false
-    t.datetime 'image_updated_at', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "shout_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shout_id"], name: "index_likes_on_shout_id"
+    t.index ["user_id", "shout_id"], name: "index_likes_on_user_id_and_shout_id", unique: true
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
-  create_table 'shouts', force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'content_type', null: false
-    t.integer 'content_id', null: false
-    t.index %w[content_type content_id], name: 'index_shouts_on_content_type_and_content_id'
-    t.index ['user_id'], name: 'index_shouts_on_user_id'
+  create_table "photo_shouts", force: :cascade do |t|
+    t.string "image_file_name", null: false
+    t.string "image_content_type", null: false
+    t.bigint "image_file_size", null: false
+    t.datetime "image_updated_at", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'text_shouts', force: :cascade do |t|
-    t.string 'body', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "shouts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "content_type", null: false
+    t.integer "content_id", null: false
+    t.index ["content_type", "content_id"], name: "index_shouts_on_content_type_and_content_id"
+    t.index ["user_id"], name: "index_shouts_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'email', null: false
-    t.string 'encrypted_password', limit: 128, null: false
-    t.string 'confirmation_token', limit: 128
-    t.string 'remember_token', limit: 128, null: false
-    t.string 'username'
-    t.index ['email'], name: 'index_users_on_email'
-    t.index ['remember_token'], name: 'index_users_on_remember_token'
-    t.index ['username'], name: 'index_users_on_username', unique: true
+  create_table "text_shouts", force: :cascade do |t|
+    t.string "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key 'shouts', 'users'
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", limit: 128, null: false
+    t.string "confirmation_token", limit: 128
+    t.string "remember_token", limit: 128, null: false
+    t.string "username"
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["remember_token"], name: "index_users_on_remember_token"
+    t.index ["username"], name: "index_users_on_username", unique: true
+  end
+
+  add_foreign_key "likes", "shouts"
+  add_foreign_key "likes", "users"
+  add_foreign_key "shouts", "users"
 end
