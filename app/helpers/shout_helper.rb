70044dbde3a96cd8c2329587db6c32ecb463c022
llabake <lemboyelabake@gmail.com>
2019-03-29 12:06:52 +0100

    move avatar_helper method to AvatarHelper

--- app/helpers/shout_helper.rb
+++ app/helpers/shout_helper.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 
 module ShoutHelper
-  def avatar(user)
-    email_digest = Digest::MD5.hexdigest(user.email)
-    gravatar_url = "//www.gravatar.com/avatar/#{email_digest}"
-
-    image_tag gravatar_url
-  end
-
   def like_button(shout)
     if current_user.liked?(shout)
       link_to 'Unlike', unlike_shout_path(shout), method: :delete
@@ -17,6 +10,6 @@ module ShoutHelper
   end
 
   def autolink(text)
-    text.gsub(/@\*+/) { |mention| link_to mention, user_path(mention[1..-1]) }.html_safe
+    text.gsub(/@\w+/) { |mention| link_to mention, user_path(mention[1..-1]) }.html_safe
   end
 end
